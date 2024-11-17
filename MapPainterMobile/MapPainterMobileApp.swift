c885a6016c84ed5d140c66b483b28eb94dcf634a
Brian P Hartnett Jr <brian.phartnettjr@gmail.com>
2024-11-16 21:49:10 -0500

    Initial Commit

---
+++ MapPainterMobile/MapPainterMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPainterMobileApp.swift
+//  MapPainterMobile
+//
+//  Created by Brian Hartnett on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MapPainterMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapPainterMobileTests/MapPainterMobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapPainterMobileTests.swift
+//  MapPainterMobileTests
+//
+//  Created by Brian Hartnett on 11/16/24.
+//
+
+import Testing
+@testable import MapPainterMobile
+
+struct MapPainterMobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
